90e3de2bd9b4c187b70137ead77dfe81b3e1541b
Dirceu Júnior <dirceujunior@MacBook-Pro-de-Dirceu.local>
2017-09-15 19:21:21 -0300

    custom select2 component. remove not used i18n

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,5 +23,41 @@ Index = {
 $(document).ready(function() {
   Index.init();
 
-  $('#id-ncm').select2();
+  //
+  $('#id-ncm').select2({
+    ajax: {
+      url: 'http://nfbr.herokuapp.com/api/ncm/?format=json&limit=10',
+      data: function (params) {
+        var query = {
+          search: params.term
+        }
+        // Query parameters will be ?search=[term]
+        return query;
+      },
+      cache: true,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Basic YWxlc3NhbmRyb0BrbXNpc3RlbWFzLmNvbS5icjpUcm9jQHIxMjM="
+      }
+    },
+    language: "pt-BR",
+    placeholder: 'Procure por um NCM',
+    templateResult: formatNCM,
+    templateSelection: formatNCMSelection,
+    escapeMarkup: function (markup) { return markup; }
+  });
 });
+
+function formatNCMSelection(ncm) {
+  return ncm.id_ncm || ncm.text;
+}
+
+function formatNCM(ncm) {
+  if (ncm.loading) {
+    return ncm.text;
+  }
+
+  var markup = ["<div class='select2-result-repository clearfix'><div><span>", ncm.codigo, "</span> <span>", ncm.descricao, "</span></div></div>"];
+
+  return markup.join('');
+}
