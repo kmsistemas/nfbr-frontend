e9734c1ce0c58f99d2e1056937108503734587b3
Dirceu Júnior <dirceujunior@MacBook-Pro-de-Dirceu.local>
2017-09-15 22:01:58 -0300

    bugged on proupose. two selects one with select2 own source code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -42,10 +42,61 @@ $(document).ready(function() {
     },
     language: "pt-BR",
     placeholder: 'Procure por um NCM',
+    escapeMarkup: function (markup) { return markup; },
     templateResult: formatNCM,
-    templateSelection: formatNCMSelection,
-    escapeMarkup: function (markup) { return markup; }
+    templateSelection: formatNCMSelection
   });
+
+
+
+  $(".js-example-data-ajax").select2({
+    ajax: {
+      url: "https://api.github.com/search/repositories",
+      dataType: 'json',
+      delay: 250,
+      data: function (params) {
+        return {
+          q: params.term, // search term
+          page: params.page
+        };
+      },
+      processResults: function (data, params) {
+        // parse the results into the format expected by Select2
+        // since we are using custom formatting functions we do not need to
+        // alter the remote JSON data, except to indicate that infinite
+        // scrolling can be used
+        params.page = params.page || 1;
+  
+        return {
+          results: data.items,
+          pagination: {
+            more: (params.page * 30) < data.total_count
+          }
+        };
+      },
+      cache: true
+    },
+    placeholder: 'Search for a repository',
+    escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+    minimumInputLength: 1,
+    templateResult: formatRepo,
+    templateSelection: formatRepoSelection
+  });
+  
+  function formatRepo(repo) {
+    if (repo.loading) {
+      return repo.text;
+    }
+  
+    var markup = ["<div class='select2-result-repository clearfix'><div><span>", repo.full_name, "</span> <span>", repo.forks_count, "</span></div></div>"];
+    
+    return markup.join('');
+  }
+  
+  function formatRepoSelection(repo) {
+    console.log(repo);
+    return repo.full_name || repo.text;
+  }
 });
 
 function formatNCMSelection(ncm) {
