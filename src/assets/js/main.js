f41a5efcbe276c60b7134e378bad31737b3ed26a
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2017-09-22 18:22:25 -0300

    Install tabs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,6 +17,15 @@ Index = {
       }
     );
 
+    // Tabs
+    var tabContainers = $('div.tabs > div');
+    $('div.tabs ul.tabNavigation a').click(function(){
+      tabContainers.hide().filter(this.hash).show();
+      $('div.tabs ul.tabNavigation a').removeClass('selected');
+      $(this).addClass('selected');
+      return false;
+    }).filter(':first').click();
+
   }
 }
 
